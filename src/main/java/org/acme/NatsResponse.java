d7253c62f0126f9942dd4b626de1e4755e294f88
Rob Ellis <github@garland.co.za>
2023-10-31 17:33:30 +0000

    Logging

--- src/main/java/org/acme/NatsResponse.java
+++ src/main/java/org/acme/NatsResponse.java
@@ -13,23 +13,21 @@ import java.util.Date;
 //@QuarkusMain
 public class NatsResponse {
 
-    private static String server = "nats://10.224.61.200:4222";
+    private static final String server = "nats://10.224.61.200:4222";
 
-    private static String subject = "greeter";
+    private static final String subject = "greeter";
 
     public static void main(String[] args) {
         try (Connection nc = Nats.connect(server)) {
             Subscription sub = nc.subscribe(subject);
+            System.out.printf("Connected to server=%s subject=%s\n",server,subject);
             while (true) {
                 Message msg = sub.nextMessage(Duration.ofMillis(500));
                 if (msg != null) {
                     String request = new String(msg.getData(), StandardCharsets.UTF_8);
                     String response = "Hi there Earthling, it's been "+ (new Date()).getTime() +" seconds since the epoch";
-                    System.out.printf("\nrequest=%s response=%s",request,response);
+                    System.out.printf("Request=%s Response=%s\n",request,response);
                     nc.publish(msg.getReplyTo(), response.getBytes());
-                    System.out.println();
-                } else {
-                    System.out.print(".");
                 }
             }
         } catch (Exception e) {
